795c80f938873d7b40aa2015563dcb929d718aa0
NikitaSaitov <nik.saitov.04@gmail.com>
2024-10-05 13:26:10 -0400

    feat: added a function to fetch all courses in a department for a given semester

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import requests
 import config
 
@@ -11,23 +11,41 @@ def home():
 @app.route('/get-subjects', methods=['GET'])
 def get_subjects():
     
-    api_url = f"https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects?apikey={config.API_KEY}"
+    endpoint = "https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects"
+    mode = "json"
+    api_url = f"{endpoint}?apikey={config.API_KEY}&mode={mode}"
 
     try:
-        # Make the request to the API
         response = requests.get(api_url)
         
         # Check if the request was successful
         if response.status_code == 200:
-            # Return the JSON response from the API
-            return jsonify(response.json())
+            return response.json()
         else:
-            # Handle errors (non-200 status codes)
             return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
     
-    except Exception as e:
-        # Handle exceptions
-        return jsonify({'error': str(e)})
+    except Exception as exc:
+        return jsonify({'error': str(exc)})
+    
+@app.route('/get-subject-info', methods=['GET'])
+def get_subject_info():
+    
+    endpoint = "https://gw.its.yale.edu/soa-gateway/courses/webservice/v3/index"
+    subject_code = "ENGL"
+    api_url = f"{endpoint}?apikey={config.API_KEY}&subjectCode={subject_code}"
+
+    try:
+        response = requests.get(api_url)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
+    
+    except Exception as exc:
+        return jsonify({'error': str(exc)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
