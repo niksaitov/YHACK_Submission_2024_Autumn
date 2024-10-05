1d4a3ebe71059a6ab422a7c6a0da602f122fa487
NikitaSaitov <nik.saitov.04@gmail.com>
2024-10-05 14:47:41 -0400

    chore: tidy up code after merge

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import json
 import requests
 import config
 import tools
-import csv
-import io
 
 app = Flask(__name__)
 
@@ -16,9 +14,10 @@ def home():
 @app.route('/get-subjects', methods=['GET'])
 def get_subjects():
 
-    response = requests.get(api_url)
+    endpoint = "https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects"
+    response = requests.get(endpoint)
     
-    # Check if the request failed
+    # check if the request failed
     if response.status_code != 200:
         return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
     else:
@@ -28,11 +27,11 @@ def get_subjects():
 def get_subject_info():
     
     endpoint = "https://gw.its.yale.edu/soa-gateway/courses/webservice/v3/index"
-    department_code = "ENGL"
+    department_code = "ENGL" #placeholder
     api_url = f"{endpoint}?apikey={config.API_KEY}&subjectCode={department_code}"
     response = requests.get(api_url)
     
-    # Check if the request was successful
+    # check if the request was successful
     if response.status_code == 200:
         return response.json()
     else:
@@ -51,7 +50,7 @@ def get_detailed_descriptions():
 
     response = requests.get(api_url)
     
-    # Check if the request failed
+    # check if the request failed
     if response.status_code != 200:
         return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
     else:
@@ -70,43 +69,6 @@ def get_detailed_descriptions():
     # df.to_csv('output.csv', index=False)
 
     return all_courses_info    
-@app.route('/get-subjects-to-csv', methods=['GET'])
-def convert_json_to_csv():
-
-    api_url = f"https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects?apikey={config.API_KEY}"
-
-    try:
-        
-        response = requests.get(api_url)
-
-        if(response.status_code == 200):
-            # populate a new csv file
-
-            # Get the JSON data from the API (assuming it's a list)
-            data = response.json()
-
-            # Define the file path where you want to save the CSV file in your repo
-            file_path = '/Users/rhou/YHACK_Submission_2024_Autumn/subjects.csv'
-
-            # Open the CSV file in write mode
-            with open(file_path, mode='w', newline='') as file:
-                writer = csv.writer(file)
-
-                # Add the CSV headers
-                writer.writerow(['Code'])
-
-                # Populate the CSV rows with data
-                for subject in data:
-                    writer.writerow([subject.get('code')])
-
-            return jsonify({'message': f'Successfully created CSV at {file_path}'})
-        else:
-            # Handle errors (non-200 status codes)
-            return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
-
-    except Exception as e:
-        # Handle exceptions
-        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
