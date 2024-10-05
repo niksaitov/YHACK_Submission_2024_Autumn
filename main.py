dcc2ccc0abe1d68e09fc61b934c0307e3db8f29e
rayconghou <rayconghou@gmail.com>
2024-10-05 12:48:49 -0400

    "convert courses to csv"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import config
 
 app = Flask(__name__)
 
@@ -10,8 +11,8 @@ def home():
 @app.route('/get-subjects', methods=['GET'])
 def get_subjects():
     
-    api_url = 'http://example.com/soa-gateway/course/webservice/v2/subjects?apikey={config.API_KEY}'
-    
+    api_url = f"https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects?apikey={config.API_KEY}"
+
     try:
         # Make the request to the API
         response = requests.get(api_url)
@@ -29,4 +30,4 @@ def get_subjects():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True)
