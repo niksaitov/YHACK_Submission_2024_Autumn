58201300d44123d0debe029e8be7d5749ac10306
NikitaSaitov <nik.saitov.04@gmail.com>
2024-10-05 15:26:52 -0400

    refactor: abtract functionality using tools.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask, jsonify
-import pandas as pd
-import json
-import requests
-import config
+from flask import Flask
 import tools
 
 app = Flask(__name__)
@@ -13,62 +9,11 @@ def home():
 
 @app.route('/get-subjects', methods=['GET'])
 def get_subjects():
-
-    endpoint = "https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects"
-    response = requests.get(endpoint)
-    
-    # check if the request failed
-    if response.status_code != 200:
-        return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
-    else:
-        return response.json()
+    return tools.get_subjects_api_call()
     
 @app.route('/get-subject-info', methods=['GET'])
 def get_subject_info():
-    
-    endpoint = "https://gw.its.yale.edu/soa-gateway/courses/webservice/v3/index"
-    department_code = "ENGL" #placeholder
-    api_url = f"{endpoint}?apikey={config.API_KEY}&subjectCode={department_code}"
-    response = requests.get(api_url)
-    
-    # check if the request was successful
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
-
-@app.route('/test') 
-def test():
-
-    return "Go fuck urself"
-
-def get_detailed_descriptions():
-
-    endpoint = "https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects"
-    mode = "json"
-    api_url = f"{endpoint}?apikey={config.API_KEY}&mode={mode}"
-
-    response = requests.get(api_url)
-    
-    # check if the request failed
-    if response.status_code != 200:
-        return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
-    else:
-        subjects = response.json()
-        unique_department_codes = tools.extract_codes(subjects)
-
-    all_courses_info = list()
-    endpoint = "https://gw.its.yale.edu/soa-gateway/courses/webservice/v3/index"
-    for departmetn_code in unique_department_codes:
-
-        api_url = f"{endpoint}?apikey={config.API_KEY}&subjectCode={departmetn_code}"
-        response = requests.get(api_url)
-        all_courses_info += response.json()
-
-    # df = pd.DataFrame(all_courses_info)
-    # df.to_csv('output.csv', index=False)
-
-    return all_courses_info    
+    return tools.get_subjects_info_api_call("ENGL")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tools.py
+++ tools.py
@@ -1,4 +1,50 @@
+from flask import jsonify
+import pandas as pd
+import requests
+import config
+
 def extract_codes(json_data):
     course_codes = {item['code'] for item in json_data}
     return list(set(course_codes))
 
+def is_faulty_response(response:dict):
+    return (response[0].keys()[0] == "error")
+
+def get_subjects_api_call():
+    endpoint = "https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects"
+    response = requests.get(endpoint)
+    
+    # check if the request failed
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
+    else:
+        return response.json()
+    
+def get_subjects_info_api_call(department_code):
+    endpoint = "https://gw.its.yale.edu/soa-gateway/courses/webservice/v3/index"
+    api_url = f"{endpoint}?apikey={config.API_KEY}&subjectCode={department_code}"
+    response = requests.get(api_url)
+    
+    # check if the request was successful
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
+    
+def get_detailed_descriptions():
+
+    subjects = get_subjects_api_call()
+    if (is_faulty_response(subjects)):
+        return(subjects)
+    
+    unique_department_codes = extract_codes(subjects)
+    all_courses_info = list()
+
+    for department_code in unique_department_codes:
+        department_sujbects_info = get_subjects_info_api_call(department_code)
+        if (is_faulty_response(department_sujbects_info)): break
+        all_courses_info += department_sujbects_info
+  
+    # df = pd.DataFrame(all_courses_info)
+    # df.to_csv('output.csv', index=False)
+    return all_courses_info    
