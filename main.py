4ef13653c6c9d2ff8ab70f843e231b67c8f7a74d
rayconghou <rayconghou@gmail.com>
2024-10-05 12:16:53 -0400

    connect flask to intersystems

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
 
-def main():
-    return "test"
+@app.route('/')
+def home():
+    return "welcome to flask"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # change host's value to your own ip address
+    # terminal command (look for some number like 192.168.x.x):
+    #   Mac: ifconfig
+    #   Windows: ipconfig
+    app.run(host='127.0.0.1', port=5000)
