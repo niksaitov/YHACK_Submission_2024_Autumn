6bb15331a61afa88b6d2ae3b2dff93642440092d
rayconghou <rayconghou@gmail.com>
2024-10-05 13:28:07 -0400

    finish converting csv

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 import requests
 import config
+import csv
+import io
 
 app = Flask(__name__)
 
@@ -10,12 +12,10 @@ def home():
 
 @app.route('/get-subjects', methods=['GET'])
 def get_subjects():
-    
-    api_url = f"https://gw.its.yale.edu/soa-gateway/course/webservice/v2/subjects?apikey={config.API_KEY}"
 
     try:
         # Make the request to the API
-        response = requests.get(api_url)
+        response = requests.get(config.api_url)
         
         # Check if the request was successful
         if response.status_code == 200:
@@ -25,6 +25,42 @@ def get_subjects():
             # Handle errors (non-200 status codes)
             return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
     
+    except Exception as e:
+        # Handle exceptions
+        return jsonify({'error': str(e)})
+    
+@app.route('/get-subjects-to-csv', methods=['GET'])
+def convert_json_to_csv():
+
+    try:
+
+        response = requests.get(config.api_url)
+
+        if(response.status_code == 200):
+            # populate a new csv file
+
+            # Get the JSON data from the API (assuming it's a list)
+            data = response.json()
+
+            # Define the file path where you want to save the CSV file in your repo
+            file_path = '/Users/rhou/YHACK_Submission_2024_Autumn/subjects.csv'
+
+            # Open the CSV file in write mode
+            with open(file_path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+
+                # Add the CSV headers
+                writer.writerow(['Code'])
+
+                # Populate the CSV rows with data
+                for subject in data:
+                    writer.writerow([subject.get('code')])
+
+            return jsonify({'message': f'Successfully created CSV at {file_path}'})
+        else:
+            # Handle errors (non-200 status codes)
+            return jsonify({'error': 'Failed to fetch subjects', 'status_code': response.status_code})
+
     except Exception as e:
         # Handle exceptions
         return jsonify({'error': str(e)})
